c4e3959adf617634f00db31c2b750ecfe5d45ff3
rue <ruvindugamage@gmail.com>
2025-11-19 12:32:37 +0200

    auth context done

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,42 @@
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../config/firebase";
+
+// creating the context
+const AuthContext = createContext();
+
+// provider component
+export const AuthProvider = ({ children }) => {
+  // global user state
+  const [user, setUser] = useState(null);
+  // waiting for firebase
+  const [loading, setLoading] = useState(true);
+
+  // listening for state changes
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  //   login function
+  const login = (email, password) =>
+    signInWithEmailAndPassword(auth, email, password);
+
+  // logout function
+  const logout = () => signOut(auth);
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
--- src/views/LogIn.jsx
+++ src/views/LogIn.jsx
@@ -1,27 +1,25 @@
 import { useState } from "react";
-import { auth } from "../config/firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { useAuth } from "../context/AuthContext";
 
 function LogIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState(null);
+  
+  const { login, user } = useAuth();
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
 
-  const logInUser = async () => {
+  const handleLogin = async () => {
     try {
-      const result = await signInWithEmailAndPassword(auth, email, password);
-      setUser(result.user.email);
-      console.log("logged in", result.user.email);
-    } catch (error) {
-      alert("invalid email or password");
-      console.log(error);
+      await login(email, password)
+    } catch(error) {
+      setError(error.message)
     }
-  };
+  }
 
   return (
     <div className="h-[30rem] mt-[5rem] w-[30rem] bg-white rounded-3xl flex flex-col items-center justify-center">
       <h1 className="text-xl font-bold">
-        {user ? `Logged in as ${user}` : "Log In to add events"}
+        Log In to add events
       </h1>
       <div className="w-full p-10 flex flex-col gap-3">
         <div>
@@ -43,8 +41,9 @@ function LogIn() {
           />
         </div>
       </div>
+      {error && <p className="text-red-500">{error}</p>}
       <button
-        onClick={logInUser}
+        onClick={handleLogin}
         className="p-2 w-30 border rounded-md cursor-pointer hover:bg-[#b4c8ff] transition-colors duration-200"
       >
         Log In
